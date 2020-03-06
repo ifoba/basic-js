adccaa17e1583b31d873a8d1a2dab47f4fd3874d
Maxim Ryabov <foba1995@gmail.com>
2020-03-06 23:30:27 +0400

    feat: solutin carbon, count and dreamteam task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string'){
+    return false;
+  }
+
+  let act = parseFloat(sampleActivity);
+  let coof = (Math.log(MODERN_ACTIVITY/act)*HALF_LIFE_PERIOD)/0.693;
+
+  if (act<=MODERN_ACTIVITY && act>0 ){
+    return Math.ceil(coof);
+  }
+  return false;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let arr = members.map(function getArr(item) {
+      if (typeof item == 'string')
+      return item.trim().toUpperCase().split('')[0];
+    })
+    return arr.sort().join('');
+  }
+  return false;
 };
