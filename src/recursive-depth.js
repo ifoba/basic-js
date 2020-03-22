f1f1953f44618649706c9d8a71834c01e51f5646
Maxim Ryabov <foba1995@gmail.com>
2020-03-22 08:25:54 +0400

    feat: add res

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (!Array.isArray(arr)) {
+            return 0;
+        }
+        let depth = 0;
+        arr.forEach(el => {
+            depth = Math.max(this.calculateDepth(el), depth);
+        });
+        return depth + 1;
     }
 };
